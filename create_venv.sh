206367fcde19989b4f8c57d28de574577c30de33
konstantinator <konstantin10000@yandex.ru>
2024-12-30 14:00:23 +0300

    add ya-lib and fix path

---
+++ create_venv.sh
@@ -0,0 +1,3 @@
+python -m venv my_env
+. ./my_env/bin/activate
+pip install -q -r requirements.txt
---
+++ load_weights.sh
@@ -0,0 +1,1 @@
+!curl -L $(yadisk-direct https://disk.yandex.ru/d/UwUd74Qzzl6H-g) -o ./pretrained_lm/best_model.pth
